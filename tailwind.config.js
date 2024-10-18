5f63aa185d2c049db874a13cbd8070c4111e56a0
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-18 22:15:03 +0100

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        select: "#FCF7FF",
+        primary: "#8576FF",
+        primaryDark: "#484554",
+        primarySelect: "#ADA9BB",
+      },
+    },
   },
   plugins: [],
 };
